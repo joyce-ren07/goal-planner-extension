b293419e93b9161d7a04be04c385ed870e58f91f
Joyce Ren <joyceren@Joyces-MacBook-Air-4.local>
2026-05-15 10:11:54 -0700

    auto: agent edit at 2026-05-15 10:11:54

--- content.js
+++ content.js
@@ -4616,8 +4616,9 @@
             }
           }
 
-          let slotIdx = resolveDomSlotIndexFromGoalRow(slotGoalRow, storageKey);
-          if (slotIdx < 0) slotIdx = resolveSlotIndexByGoalChipsOnCalendar(chip, slotGoalRow);
+          let slotIdx = readChipSlotIndexFromDataset(chip, allowed.length);
+          if (slotIdx < 0) slotIdx = resolveDomSlotIndexFromGoalRow(slotGoalRow, storageKey);
+          if (slotIdx < 0) slotIdx = resolveSlotIndexByGoalChipsOnCalendar(chip, slotGoalRow, legacyGoals);
           if (slotIdx < 0) slotIdx = resolveSlotIndexByAnchorTime(chip, slotGoalRow);
           if (slotIdx < 0) {
             slotIdx = await resolveSlotIndexForGoalToggle(
