04c37a2ef8fc605965960c456ee5594b7e901e3b
Joyce Ren <joyceren@Joyces-MacBook-Air-4.local>
2026-05-14 20:39:49 -0700

    auto: agent edit at 2026-05-14 20:39:49

--- content.js
+++ content.js
@@ -1160,9 +1160,6 @@
 
     root.style.removeProperty('--gp-native-root-ml');
     root.style.removeProperty('--gp-native-root-mr');
-    root.style.removeProperty('--gp-native-icon-fvs');
-    root.style.removeProperty('--gp-native-add-icon-fvs');
-    root.style.removeProperty('--gp-native-title-font-weight');
 
     const setVar = (name, val) => {
       if (val == null || val === '' || val === 'auto' || val === 'normal') return;
