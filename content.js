f4aa6e01af9297512e8305d15ec6faf35de8d57e
Joyce Ren <joyceren@Joyces-MacBook-Air-4.local>
2026-05-14 18:22:48 -0700

    auto: agent edit at 2026-05-14 18:22:48

--- content.js
+++ content.js
@@ -336,7 +336,9 @@
 
     wireEvents();
     setupMyGoalsUnifiedBinding();
+    setupLeftSidebarGoalsMountObserver();
     renderHomeScreen();
+    scheduleLeftSidebarGoalsMountAttempts();
     setupCalendarPushObserver();
     setupNativeSidebarObserver();
     setupGoalEventObserver();
