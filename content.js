fbd66824d5773230f5f144148081d141d0556f1e
Joyce Ren <joyceren@Joyces-MacBook-Air-4.local>
2026-05-16 03:29:14 -0700

    auto: agent edit at 2026-05-16 03:29:14

--- content.js
+++ content.js
@@ -6604,7 +6604,6 @@
     ul.style.cssText = 'list-style:none;margin:0;padding:0;width:100%;';
     gpGdPaintSubtaskList(ul, subtasks);
 
-    stCol.appendChild(stHead);
     stCol.appendChild(emptySt);
     stCol.appendChild(ul);
 
